c4dd738433973afddd2ea6215608be13f6c0ebb3
Jarrel Koon <2601888@sit.singaporetech.edu.sg>
2026-09-04 11:40:29 +0800

    Assignment Operators

--- hello.py
+++ hello.py
@@ -7,6 +7,15 @@ username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
+followers += 50  # Increase followers by 50
+print("Day 1:", followers)
+
+followers +=20 #Increase followers by 20
+print("Day 2:", followers)
+
+followers -= 10  # Decrease followers by 10
+print("Day 3:", followers)
+
 print("Username:", username)
 print("Bio:", bio) 
 print("Followers:", followers)
